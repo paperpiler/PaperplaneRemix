dfd3573eaac3ebf19fd9f0a860827602396b1e27
Udit Karode <udit.karode@gmail.com>
2020-03-17 15:41:57 +0530

    plugins: tgr: dynamically obtain id and ref

--- userbot/plugins/tgr.py
+++ userbot/plugins/tgr.py
@@ -2,11 +2,14 @@
 # @Purpose Forwards a message to the TGR channel along with the divider sticker
 # @Date 03/17/2020
 # @License Apache License, Version 2.0 <https://www.apache.org/licenses/LICENSE-2.0.txt>
+import os
+
 from telethon.tl.functions.messages import SendMediaRequest
 from telethon.tl.types import InputMediaDocument, InputDocument
 from userbot import client
 import random
 from userbot.utils.events import NewMessage
+from telethon.tl.functions.messages import GetStickersRequest
 
 SUCCESS_MESSAGES = [
     "Retard added to showcase!",
@@ -14,7 +17,7 @@ SUCCESS_MESSAGES = [
     "The rarts have been returned to where they belong!"
 ]
 
-tgr_entity = "t.me/yeet_retards"
+tgr_entity = client.get_entity("t.me/yeet_retards")
 
 
 @client.onMessage(command="tgr",
@@ -35,13 +38,21 @@ async def tgr_send(event: NewMessage.Event) -> None:
         from_peer=reply.chat_id
     )
 
+    divider = client(GetStickersRequest(
+        emoticon='❤',
+        hash=-1447283942124334054
+    ))
+
     # Send the divider sticker
     await client(SendMediaRequest(
         entities=tgr_entity,
+        message='',
+        peer=client.get_entity("hyperterminal"),
         media=InputMediaDocument(
             id=InputDocument(
-                id="CAACAgUAAxkBAAJVU15v0r-xxrxT6e0fgTb76TgdL0fUiAAKxAAN24FYTGlSNNOw26usYBA",
-                access_hash="-1447283942124334054"
+                id=divider.id,
+                access_hash=divider.access_hash,
+                file_reference=divider.file_reference
             )
         )
     ))
